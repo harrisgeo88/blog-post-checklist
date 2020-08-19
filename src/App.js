afd0eb419a8f027791a0a8e041dbfc31ce321a16
Harris Geo <charilaoss@gmail.com>
2020-08-19 21:37:23 +0100

    Github badges added 🙌

--- src/App.js
+++ src/App.js
@@ -2,18 +2,47 @@ import React from 'react'
 import { Checkbox } from './components/Checkbox'
 import Emoji from 'a11y-react-emoji'
 import lang from './lang/en-gb.json'
+import GithubCorner from 'react-github-corner'
 import './App.css'
+import 'react-github-button/assets/style.css'
+
+const GitHubButton = require('react-github-button')
 
 const App = () => {
+  const aProps = {
+    target: '_blank',
+    rel: 'noopener noreferrer',
+  }
   const { checklist, tips, tipsList, title, quote, list } = lang
   return (
     <div className="App">
       <header className="banner">
         <div className="banner-container">
+          <GithubCorner
+            href="https://github.com/harrisgeo88/blog-post-checklist/"
+            bannerColor="#fafffd"
+            octoColor="#3c91e6"
+            {...aProps}
+          />
           <h1 className="header-1">
             <Emoji symbol="✍🏻" label="writting hand" /> {title}
           </h1>
           <p id="quote">{quote}</p>
+          <div id="github-badges">
+            <GitHubButton
+              type="stargazers"
+              size="large"
+              namespace="harrisgeo88"
+              repo="blog-post-checklist"
+            />
+            <GitHubButton
+              className="hallo"
+              size="large"
+              type="forks"
+              namespace="harrisgeo88"
+              repo="blog-post-checklist"
+            />
+          </div>
         </div>
       </header>
       <div className="main-body">
@@ -35,12 +64,7 @@ const App = () => {
         </ul>
       </div>
       <footer>
-        <a
-          id="created-by"
-          href="https://harrisgeo.me"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a id="created-by" href="https://harrisgeo.me" {...aProps}>
           Created by Harris Geo
         </a>
       </footer>
