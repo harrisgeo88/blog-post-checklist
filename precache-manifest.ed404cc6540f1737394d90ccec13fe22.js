adfcc1850975954c81afe1aaa557cfefbacd1e40
Harris Geo <charilaoss@gmail.com>
2020-08-21 08:00:50 +0100

    Updates

--- precache-manifest.ed404cc6540f1737394d90ccec13fe22.js
+++ precache-manifest.ed404cc6540f1737394d90ccec13fe22.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dac810a400b3ec2103a9a2e341843fca",
+    "revision": "1bdf7dfc6f533e54c77365f4f3e5561c",
     "url": "/blog-post-checklist/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/blog-post-checklist/static/css/2.9538c45e.chunk.css"
   },
   {
-    "revision": "e40c6fe6e86061615451",
+    "revision": "c710d61affd322571b1b",
     "url": "/blog-post-checklist/static/css/main.cad6b096.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/blog-post-checklist/static/js/2.68fa8a7e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e40c6fe6e86061615451",
-    "url": "/blog-post-checklist/static/js/main.9dd051ec.chunk.js"
+    "revision": "c710d61affd322571b1b",
+    "url": "/blog-post-checklist/static/js/main.04b7b11a.chunk.js"
   },
   {
     "revision": "294d93f7219cf80af3ef",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blog-post-checklist/precache-manifest.5815baa03519e75be7919ef671c96d40.js"
+  "/blog-post-checklist/precache-manifest.ed404cc6540f1737394d90ccec13fe22.js"
 );
 
 self.addEventListener('message', (event) => {
